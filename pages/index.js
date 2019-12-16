9ef569cd74482cbd11803973f5505890d67aec49
Durul Doktoroğlu <durul@sufle.io>
2019-12-17 01:18:33 +0300

    edit content

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <div>Welcome to my awesome page!</div>
+    return <div>
+        Welcome to my awesome page!
+        <p>This is a fantastic page.</p>
+    </div>
   }
   
   export default Home
